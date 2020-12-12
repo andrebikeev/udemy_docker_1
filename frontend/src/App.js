be3e29117be7e620158f0913bc7253a5452bbd56
Bikeev Andrey <andre.bikeev@gmail.com>
2020-12-12 16:39:28 +0500

    last lesson

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const makeapirequest = () => {
+    console.log('makeapirequest')
+    axios("/api/testwithcurrentuser").then(response => {
+      console.log("response", response);
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          We are in dev!
+          Hello from websockets!
         </p>
         <a
           className="App-link"
@@ -19,6 +27,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <button onClick={makeapirequest}>Make api request</button>
     </div>
   );
 }
